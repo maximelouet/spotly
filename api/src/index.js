a226af558dd1f9a739f85ee5330ca97b7cf843cd
Maxime Louet <maxime@saumon.io>
2020-04-24 11:54:10 +0100

    Dockerize API

--- api/src/index.js
+++ api/src/index.js
@@ -32,7 +32,7 @@ fastify.setNotFoundHandler(async (request, reply) => {
 
 const start = async () => {
   try {
-    await fastify.listen(3001);
+    await fastify.listen(3001, '0.0.0.0');
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
