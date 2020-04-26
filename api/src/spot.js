3177ddf2895c7783a6bc689c8dfdcd7957f964d1
Maxime Louet <maxime@saumon.io>
2020-04-26 11:32:41 +0100

    Guess API redirect_uri with FRONT_URL

--- api/src/spot.js
+++ api/src/spot.js
@@ -8,7 +8,7 @@ class Spot {
     return new SpotifyWebApi({
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      redirectUri: process.env.SPOTIFY_REDIRECT_URI,
+      redirectUri: `${process.env.FRONT_URL.replace(/\/$/, '')}/callback`,
       ...params,
     });
   }
