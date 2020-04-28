7cb516ffd2ee499b584baa564275ea489366598e
Maxime Louet <maxime@saumon.io>
2020-04-28 11:38:10 +0100

    Fix mobile lyrics fetching
    It seems lyric sites change responses based on the User-Agent. We now
    return the default (Chrome-based) user agent is we detect the user is on
    mobile.

--- api/src/LyricsHelper/index.js
+++ api/src/LyricsHelper/index.js
@@ -4,8 +4,9 @@ import fetchFromGoogle from './sources/google';
 import cleanSongName from './cleanSongName';
 
 const computeRequestHeaders = (clientHeaders) => {
+  const userAgent = (clientHeaders['user-agent'] && !clientHeaders['user-agent'].match(/mobile/gi)) ? clientHeaders['user-agent'] : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36';
   return {
-    'user-agent': clientHeaders['user-agent'] || 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36',
+    'user-agent': userAgent,
     'accept-language': clientHeaders['accept-language'] || 'en-GB,en;q=0.8,fr-FR;q=0.5,fr;q=0.3',
     accept: 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
     'upgrade-insecure-requests': 1,
